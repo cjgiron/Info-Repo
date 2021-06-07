f8e1fa7cc5e385be6bfcc4eb2cf1e35b466e98d5
Ciara Giron <ciarajg@yahoo.com>
2021-06-07 12:27:06 -0700

    My first commit

---
+++ Info.java
@@ -0,0 +1,8 @@
+public class Info {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Ciara Giron");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is American Canyon, CA");
+    }
+}
