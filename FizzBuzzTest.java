5ba39e22ddba6950e3f6b668b40ec09762380515
Ciara J. Giron <65361904+cjgiron@users.noreply.github.com>
2021-06-07 14:57:03 -0700

    FizzBuzz

---
+++ FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest {
+
+    public static void main(String[] args) {
+        FizzBuzz iD = new FizzBuzz();
+        String answer = iD.fizzBuzz(6);
+        System.out.println(answer);
+    }
+}
