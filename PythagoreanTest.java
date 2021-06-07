01117e8d88a3d4b8d1ed1342c3ea6969400a58f9
Ciara J. Giron <65361904+cjgiron@users.noreply.github.com>
2021-06-07 14:11:58 -0700

    Pythagorean/PythagoreanTest

---
+++ PythagoreanTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTest {
+
+    public static void main(String[] args) {
+        Pythagorean iD = new Pythagorean();
+        double legC = iD.calculateHypotenuse(4, 3);
+        System.out.println(legC);
+    }
+}
